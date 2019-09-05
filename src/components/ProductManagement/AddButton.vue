a535520f143defd6751ac1accaa21e299f9d66c2
Ankur Nigam <ankur.nigam198@gmail.com>
2019-09-05 15:40:39 +0530

    addButton

---
+++ src/components/ProductManagement/AddButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn
+    fab
+    large
+    color="primary"
+    bottom
+    right
+    fixed
+    @click.native.stop="click"
+  >
+    <v-icon>mdi-plus</v-icon>
+  </v-btn>
+</template>
+<script>
+export default {
+  methods: {
+    click() {
+      this.$emit("Add");
+    }
+  }
+};
+</script>
