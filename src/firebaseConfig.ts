a07137ff6dbb55f4aeeac9c1dbf7a6d549eca4c5
Ankur Nigam <ankur.nigam198@gmail.com>
2019-09-12 17:21:52 +0530

    added proper security rules so no need to hide config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAT_dNU866al3UFX0mGYff3fi4zGsL0HwY",
+  authDomain: "indianchaicompany.firebaseapp.com",
+  databaseURL: "https://indianchaicompany.firebaseio.com",
+  projectId: "indianchaicompany",
+  storageBucket: "indianchaicompany.appspot.com",
+  messagingSenderId: "912570061796",
+  appId: "1:912570061796:web:7d7f5fbd8e94bf48bd2df1"
+};
